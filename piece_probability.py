d08a410bb2711df5feff0f21261eca2d01648f65
wouterstultiens <wouterstultiens@gmail.com>
2024-10-22 13:21:14 +0200

    notation fixed

--- piece_probability.py
+++ piece_probability.py
@@ -10,16 +10,16 @@ pieces_folder = 'pieces'
 # List of all piece image names in logical order
 pieces = ['bK', 'bQ', 'bR', 'bB', 'bN', 'bP', 'wK', 'wQ', 'wR', 'wB', 'wN', 'wP', 'empty']
 
-# Mapping from file numbers to board coordinates
+# Mapping from file numbers to board coordinates after a 270-degree clockwise (or 90-degree counterclockwise) rotation
 board_positions = {
-    0: "a8", 1: "b8", 2: "c8", 3: "d8", 4: "e8", 5: "f8", 6: "g8", 7: "h8",
-    8: "a7", 9: "b7", 10: "c7", 11: "d7", 12: "e7", 13: "f7", 14: "g7", 15: "h7",
-    16: "a6", 17: "b6", 18: "c6", 19: "d6", 20: "e6", 21: "f6", 22: "g6", 23: "h6",
-    24: "a5", 25: "b5", 26: "c5", 27: "d5", 28: "e5", 29: "f5", 30: "g5", 31: "h5",
-    32: "a4", 33: "b4", 34: "c4", 35: "d4", 36: "e4", 37: "f4", 38: "g4", 39: "h4",
-    40: "a3", 41: "b3", 42: "c3", 43: "d3", 44: "e3", 45: "f3", 46: "g3", 47: "h3",
-    48: "a2", 49: "b2", 50: "c2", 51: "d2", 52: "e2", 53: "f2", 54: "g2", 55: "h2",
-    56: "a1", 57: "b1", 58: "c1", 59: "d1", 60: "e1", 61: "f1", 62: "g1", 63: "h1",
+    0: "a1", 1: "a2", 2: "a3", 3: "a4", 4: "a5", 5: "a6", 6: "a7", 7: "a8",
+    8: "b1", 9: "b2", 10: "b3", 11: "b4", 12: "b5", 13: "b6", 14: "b7", 15: "b8",
+    16: "c1", 17: "c2", 18: "c3", 19: "c4", 20: "c5", 21: "c6", 22: "c7", 23: "c8",
+    24: "d1", 25: "d2", 26: "d3", 27: "d4", 28: "d5", 29: "d6", 30: "d7", 31: "d8",
+    32: "e1", 33: "e2", 34: "e3", 35: "e4", 36: "e5", 37: "e6", 38: "e7", 39: "e8",
+    40: "f1", 41: "f2", 42: "f3", 43: "f4", 44: "f5", 45: "f6", 46: "f7", 47: "f8",
+    48: "g1", 49: "g2", 50: "g3", 51: "g4", 52: "g5", 53: "g6", 54: "g7", 55: "g8",
+    56: "h1", 57: "h2", 58: "h3", 59: "h4", 60: "h5", 61: "h6", 62: "h7", 63: "h8",
 }
 
 # Function to compare two images using SSIM and return a similarity score
