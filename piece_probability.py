81f25ffb1b872f02b82f0b9db3339674ff87d8de
wouterstultiens <wouterstultiens@gmail.com>
2024-10-22 21:09:08 +0200

    refactored working version

--- piece_probability.py
+++ piece_probability.py
@@ -11,9 +11,17 @@ pieces_folder = 'pieces'
 pieces = ['bK', 'bQ', 'bR', 'bB', 'bN', 'bP', 'wK', 'wQ', 'wR', 'wB', 'wN', 'wP', 'empty']
 
 # Board positions from white's perspective
-board_positions_white = [f"{col}{row}" for row in range(1, 9) for col in 'abcdefgh']
+board_positions_white = [
+    "a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8",
+    "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8",
+    "c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8",
+    "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8",
+    "e1", "e2", "e3", "e4", "e5", "e6", "e7", "e8",
+    "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8",
+    "g1", "g2", "g3", "g4", "g5", "g6", "g7", "g8",
+    "h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8"
+]
 
-# Board positions from black's perspective
 board_positions_black = board_positions_white[::-1]
 
 def compare_images(imageA, imageB):
